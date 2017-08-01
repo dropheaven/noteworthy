27a73d5442481852cb9dfcc91e71f4889ee0b2dd
Saad <Dropheaven@live.com>
2017-08-01 18:56:17 -0400

    users are able to create new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,33 @@
 class ArticlesController < ApplicationController
 
   def index
+    @articles = Article.all
+  end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @user = current_user
+    @article = Article.new
+  end
+
+  def create
+    @user = current_user
+    @article = @user.articles.build(article_params)
+
+    if @article.save
+      redirect_to user_article_path(current_user, @article)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :link)
   end
 
 end
