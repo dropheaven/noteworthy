7df2d6dab1e86cb7ee8e139f259b049f16f552e0
Saad <Dropheaven@live.com>
2017-08-04 12:21:46 -0400

    updated basic validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ActiveRecord::Base
   has_many :article_categories
   has_many :articles, through: :article_categories
+	
+	before_save { name.downcase! }
+	validates :name, presence: true,
+									 uniqueness: true
+	
 end
